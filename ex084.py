751fe910f16e9d9e9dd2aab841baeb2cec6faf0c
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-08 00:12:49 -0300

    exercicio84

---
+++ ex084.py
@@ -0,0 +1,49 @@
+#for thiago 08/03/2022
+#definir as listas globais
+dados = list()
+pessoa = list()
+pesado = leve = 0
+
+#dados digitados pelo usuario
+while True:
+
+    print('='*30)
+    pessoa.append(str(input('Nome: ')))
+    pessoa.append(float(input('Peso: ')))
+    dados.append(pessoa[:])
+
+    if len(dados) == 1:
+        pesado = leve = pessoa[1]
+
+    if pessoa[1] > pesado:
+        pesado = pessoa[1]
+
+    if pessoa[1] < leve:
+        leve = pessoa[1]
+
+
+    pessoa.clear()
+    
+    continua = str(input('Deseja continuar? [S/N]: ')).strip().upper()
+
+    while continua not in 'SN': 
+
+        continua = str(input('Deseja continuar? [S/N]: ')).upper().strip()
+
+    if continua == 'N':
+        break
+
+print('='*30)
+print(f'Existem {len(dados)} pessoas cadastradas!')
+
+print(f'Maior valor registrado: {pesado} que sao de: ', end=' ')
+
+for p in dados:
+    if p[1] == pesado:
+        print([p[0]], end=' ')
+
+print(f'\nMenor valor registrado: {leve} que sao de: ', end=' ')
+for p in dados:
+    if p[1] == leve:
+        print([p[0]], end=' ')
+print()

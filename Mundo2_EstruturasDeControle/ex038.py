57bddb7b5e3244060383f8268fd142e19e0c8db0
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-10 14:37:16 -0300

    Comparando números

--- Mundo2_EstruturasDeControle/ex038.py
+++ Mundo2_EstruturasDeControle/ex038.py
@@ -1,14 +1,29 @@
+'''
+Desafio 38
 
+Problema: Escreva um programa que leia dois números inteiros e compare-os.
+mostrando na tela uma mensagem:
+
+– O primeiro valor é maior
+
+– O segundo valor é maior
+
+– Não existe valor maior, os dois são iguais
+
+Resolucao do problema: 
+'''
 
 n1 = int(input('Digite O Primeiro Numero: '))
 n2 = int(input('Digite O Segundo Numero: '))
 
 if n1 > n2: 
-    print(f'O Maior Valor E {n1}')
+
+    print(f'O Maior Valor digitado foi: {n1}')
 
 elif n1 == n2: 
-    print(f'Nao Existe Valor Maior Ou Menor Os Dois Sao Iguais!')
+
+    print(f'Nao Existe Valor MAIOR Ou MENOR Os Dois Sao Iguais!')
 
 else: 
-    print(f'O Maior Valor E {n2}')
 
+    print(f'O Maior Valor Digitado foi: {n2}')

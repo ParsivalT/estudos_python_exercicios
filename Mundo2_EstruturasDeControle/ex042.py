837d92b00dee385df4efddd93ad897fc5eecdaa6
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-10 15:46:09 -0300

    Analisando Triângulos v2.0

--- Mundo2_EstruturasDeControle/ex042.py
+++ Mundo2_EstruturasDeControle/ex042.py
@@ -1,7 +1,21 @@
+'''
+Desafio 42
+
+Problema: Refaça o DESAFIO 35 dos triângulos, acrescentando o recurso de mostrar
+          que tipo de triângulo será formado:
+
+            – EQUILÁTERO: todos os lados iguais
+
+            – ISÓSCELES: dois lados iguais, um diferente
+
+            – ESCALENO: todos os lados diferentes
+
+Resolucao de problemas:
+'''
 l1 = float(input('Digite O Primeiro Valor: '))
 l2 = float(input('Digite O Segundo Valor: '))
 l3 = float(input('Digite O Terceiro Valor:'))
-
+print('--'*20)
 if l3 < l1 + l2 and l2 < l3 + l1 and l1 < l3 + l2:
     print('Os Segmentos acima Podem Formar um Triangulo ', end='')
 

44de6b5c26fb04149ae50080d383393119b67ea1
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 13:40:47 -0300

    Progressão Aritmética v2.0

--- Mundo2_EstruturasDeControle/ex061.py
+++ Mundo2_EstruturasDeControle/ex061.py
@@ -1,5 +1,11 @@
-#mostra uma PA 
+'''
+Desafio 61
 
+Problema: Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, 
+          mostrando os 10 primeiros termos da progressão usando a estrutura while.
+          
+Problema:
+'''
 print('-=-'*30)
 primeiro = int(input('Digite O Primeiro termo: '))
 razao = int(input('Digite A Razao: '))

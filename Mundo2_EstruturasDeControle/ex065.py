d908b44e1960c9d077a75c54a1764e848f985f83
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 14:20:50 -0300

    Maior e Menor valores

--- Mundo2_EstruturasDeControle/ex065.py
+++ Mundo2_EstruturasDeControle/ex065.py
@@ -1,43 +1,50 @@
-# soma todos os valores digitados, e mostra a media, maior e menor valor digitado!
+'''
+Desafio 65
+
+Problema: Crie um programa que leia vários números inteiros pelo teclado. 
+          No final da execução, mostre a média entre todos os valores e qual 
+          foi o maior e o menor valores lidos. O programa deve perguntar ao usuário 
+          se ele quer ou não continuar a digitar valores.
+          
+Resolucao do problema: 
+'''
 from os import system 
 
 numero = cont = maior = menor = total =  0 
-continuar = ''
 
-while True:
+while True:print(maior, menor ) #testeprint(maior, menor ) #testeprint(maior, menor ) #teste
     
-    print('-=-'*10)
-
     system('clear')
+    print('='*20)
+
     cont += 1
     numero = int(input('Digite um numero: '))
     continuar = str(input('Deseja Continuar? [S/N]: ')).upper()
     total += numero
     
     if cont == 1:
-
         maior = numero
         menor = numero 
-        print(maior, menor ) #teste
 
     if numero > maior:
-
         maior = numero
 
     if numero < menor:
-
         menor = numero
 
     while continuar not in 'SN':
         
         system('clear')
         print('**'*10)
-
         print('Por favor Digite uma opcao valida!')
+
         continuar = str(input('Deseja Continuar? [S/N]: ')).upper()
         
-
     if continuar == 'N':
         break
+
+system('clear')
+print('=='*24)
 print(f'\033[1mA media dos valores digitado E: {total/cont:.1f}')
 print(f'O maior numero digitado foi {maior} ja o menor foi: {menor}\033[m')
+print('=='*24)

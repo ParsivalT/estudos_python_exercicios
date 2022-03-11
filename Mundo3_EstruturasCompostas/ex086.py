2a08aa65d43e63696c04b7b12ce000ef3737e38d
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 20:57:46 -0300

    Matriz em Python

--- Mundo3_EstruturasCompostas/ex086.py
+++ Mundo3_EstruturasCompostas/ex086.py
@@ -1,4 +1,4 @@
-"""
+'''
 Desafio 086
 
 Problema: Crie um programa que crie uma matriz 3x3 e preecha com valores lidos
@@ -6,11 +6,12 @@ Problema: Crie um programa que crie uma matriz 3x3 e preecha com valores lidos
           no final, mostre a matriz na tela, com a formatacao correta
           
 Resolucao do problema: 
-"""
+'''
 matriz = [[],[],[]]
 
 #Alimentando a lista com valores.
 for coluna in range(0, 3):
+
     for linha in range(0,3):
         matriz[coluna].append(int(input(f'Digite o valor para a posicao [{coluna},{linha}]: ')))
 
@@ -18,7 +19,8 @@ print('='*30)
 
 #Exibindo a mensagem no console.
 for coluna in range(0,3):
-    for linha in range(0,3):
-        print(f'[ {matriz[coluna][linha]:9^} ]', end=' ')
-    print()    
 
+    for linha in range(0,3):
+        print(f'[ {matriz[coluna][linha]:6^} ]', end=' ')
+
+    print()    

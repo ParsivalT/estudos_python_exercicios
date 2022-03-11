147d13f69dd3eb5c51c4dab31eac1edf5f5d666f
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 02:41:59 -0300

    Progressão Aritmética

--- Mundo2_EstruturasDeControle/ex051.py
+++ Mundo2_EstruturasDeControle/ex051.py
@@ -1,10 +1,19 @@
+'''
+Desafio 51
+
+Problema:  Desenvolva um programa que leia o primeiro termo e a razão de uma PA. 
+           No final, mostre os 10 primeiros termos dessa progressão.
+           
+Resolucao do problema:
+'''
 print('-='*30)
 print('>>>>>>>> 10 Termos de uma PA <<<<<<<<')
 print('~'*20)
+
 a1 = int(input('Primeiro Termo: '))
 razao = int(input('digite A Razao: '))
-
 ac = 0
+
 for c in range(0,10):
    ac = a1 + razao * c
    print(f'{ac}', end = ' - ')    

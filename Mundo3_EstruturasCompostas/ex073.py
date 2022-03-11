969222cf7f950c817c6a92041f365c7b803c1200
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 19:59:50 -0300

    Tuplas com Times de Futebol

--- Mundo3_EstruturasCompostas/ex073.py
+++ Mundo3_EstruturasCompostas/ex073.py
@@ -1,3 +1,20 @@
+'''
+Desafio 73
+
+Problema:  73: Crie uma tupla preenchida com os 20 primeiros colocados 
+           da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. 
+           Depois mostre:
+
+                a) Os 5 primeiros times.
+
+                b) Os últimos 4 colocados.
+
+                c) Times em ordem alfabética.
+
+                d) Em que posição está o time da Chapecoense.
+                
+Resolucao do problema: 
+'''
 times = ('America-MG', 'Athletico-PR', 'Atletico-GO', 'Atletico-MG',
          'Avai', 'Botafogo', 'Ceara SC', 'Corinthias', 'Coritiba',
          'Cuiaba', 'Flamengo', 'Fluminense', 'Fortaleza', 'Goias',
@@ -13,4 +30,3 @@ print(f'Em ordem alfabetica: {sorted(times)}')
 print('='*46)
 print('O Avai esta na posicao {}'.format(times.index('Avai')))
 print('='*46)
-

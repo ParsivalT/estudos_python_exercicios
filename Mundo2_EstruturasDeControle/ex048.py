bc1f1301546e453a98544a1c3a78495000fd58de
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 02:26:39 -0300

    Soma ímpares múltiplos de três

--- Mundo2_EstruturasDeControle/ex048.py
+++ Mundo2_EstruturasDeControle/ex048.py
@@ -1,6 +1,16 @@
+'''
+Desafio 48
+
+Problema: Faça um programa que calcule a soma entre todos os números 
+          que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+          
+Resolucao do problema:
+'''
 multiplos = 0
+
 for c in range(0, 501):
     i = c % 2 
+    
     if i == 0 and i % 3 == 0:
         multiplos += c 
 

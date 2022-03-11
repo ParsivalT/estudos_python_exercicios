7dc4b3cdca5be0d97091a437ef39a01f38645af8
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 20:21:37 -0300

    Contando vogais em Tupla

--- Mundo3_EstruturasCompostas/ex077.py
+++ Mundo3_EstruturasCompostas/ex077.py
@@ -1,7 +1,11 @@
 '''
 Desafio 77
 
-Problema:'''
+Problema:  Crie um programa que tenha uma tupla com várias palavras (não usar acentos). 
+           Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+           
+Resolucao do problema:
+'''
 palavras = ('python', 'caxinha', 'agenda', 'pokemon', 'avenida')
 vogais = ('a', 'e', 'i', 'o', 'u')
 palavra = ''

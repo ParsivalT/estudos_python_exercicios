eed52acc75b47b2f63b843d0c97265b9a787d325
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 02:53:52 -0300

    Grupo da Maioridade

--- Mundo2_EstruturasDeControle/ex054.py
+++ Mundo2_EstruturasDeControle/ex054.py
@@ -1,8 +1,18 @@
+'''
+Desafio 54
+
+Problema:  Crie um programa que leia o ano de nascimento de sete pessoas. 
+           No final, mostre quantas pessoas ainda não atingiram a maioridade 
+           e quantas já são maiores.
+
+Resolucao do problema:           
+'''
 maior = 0
 menor = 0
 
 for c in range(0, 8):
     i = int(input('Digite A Idade Da Pessoa: '))
+    
     if i >= 18:
         maior += 1
 

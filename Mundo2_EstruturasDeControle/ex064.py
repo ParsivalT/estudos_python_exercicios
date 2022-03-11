dae5a9a1ef05ab4c7ac1d12a13a41d41f9510f9e
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 14:10:00 -0300

    Tratando vários valores v1.0

--- Mundo2_EstruturasDeControle/ex064.py
+++ Mundo2_EstruturasDeControle/ex064.py
@@ -1,5 +1,13 @@
-# guarda os numeros digitados e depois os somalos 
+'''
+Desafio 64 
 
+Problema: Crie um programa que leia vários números inteiros pelo teclado. 
+          O programa só vai parar quando o usuário digitar o valor 999, que 
+          é a condição de parada. No final, mostre quantos números foram digitados 
+          e qual foi a soma entre eles (desconsiderando o flag).
+          
+Resolucao do problema: 
+'''
 print('-=-'*20)
 numero = int(input('Digite um numero[999 para o programa]: '))
 soma = 0
@@ -10,11 +18,11 @@ while numero != 999:
         break
 
     else:
-
         soma += numero
         print('--'*10)
         
         numero = int(input('Digite um numero[999 para o programa]: '))
+
 print('-=-'*20)       
 print(f'a Soma dos numeros digitados E: {soma}')
 print('-=-'*20)

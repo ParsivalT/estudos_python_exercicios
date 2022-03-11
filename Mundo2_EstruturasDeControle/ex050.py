c8b8d7160a8779c8e360d451e5f745895414b275
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 02:35:57 -0300

    Soma dos pares

--- Mundo2_EstruturasDeControle/ex050.py
+++ Mundo2_EstruturasDeControle/ex050.py
@@ -1,8 +1,18 @@
+'''
+Desafio 50
+
+Probleme:  Desenvolva um programa que leia seis números inteiros e mostre
+           a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+           
+Resolucao do problema:
+'''
 impar = 0
 contador = 0 
+
 for c in range(0,7):
     n = int(input('Digite Um numero: '))
     i = n % 2
+
     if i == 0:
         contador += 1
         impar += n 

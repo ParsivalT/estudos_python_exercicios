cc006e197f9d4ab6e8e31c9ee96115122e4da166
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 00:11:14 -0300

    Gerenciador de Pagamentos

--- Mundo2_EstruturasDeControle/ex044.py
+++ Mundo2_EstruturasDeControle/ex044.py
@@ -1,8 +1,19 @@
-'''avista dinheiro/cheque:10% de desconto
-avista no cartao:5% de desconto 
-em 2x no cartao preco normal 
-3x ou mais no cartao 20% de juros '''
+'''
+Desafio 44
 
+Problema: Elabore um programa que calcule o valor a ser pago por um produto,
+          considerando o seu preço normal e condição de pagamento:
+
+                – à vista dinheiro/cheque: 10% de desconto
+
+                – à vista no cartão: 5% de desconto
+
+                – em até 2x no cartão: preço formal 
+
+                – 3x ou mais no cartão: 20% de juros 
+                
+Resolucao do problema:
+'''
 print('-=-'*20)
 p = float(input('Digite O Preco Do Produto: R$'))
 print('~'*30)
@@ -13,26 +24,33 @@ print('''Escolha A Forma De Pagameto desejado(a)
 (1) Avista Dinheiro/Cheque
 (2) Avista no Cartao 
 (3) Parcelado no Cartao ''')
+
 print('~'*30)
 f = int(input(':'))
 print('~'*30)
 
 if f == 3:
+    
     print('Deseja Parcelar Em Quantas vezes ?')
     pa = int(input('x'))
+    
     if pa <= 2:
+
         t = p / pa
         print(f'Total {pa} parcelas de  R${t:.2f}')
 
     else:
+
         t =(p + p*20/100) / pa
         print(f'Total {pa} parcelas de R${t:.2f}')
 
 elif f == 2:
+
     t = (p-(p*5/100))
     print(f'O Valor Do Produto De R${p:.2f} foi para R${t:.2f}')
 
 else:
+
     t = (p-(p*10/100))
     print('com 10% de DESCONTO')
     print(f'O Valor Do Produto De R${p:.2f} foi para R${t:.2f} ')

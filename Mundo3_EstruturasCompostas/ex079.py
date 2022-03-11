9abd626ec8087f2e4128b890505fe367de66b5b3
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 20:29:38 -0300

    Valores únicos em uma Lista

--- Mundo3_EstruturasCompostas/ex079.py
+++ Mundo3_EstruturasCompostas/ex079.py
@@ -1,12 +1,20 @@
-#algoritimo que guarde quantos valores o usuario quiser em uma lista 
-#  for: Thiago 05/03/2022
+'''
+Desafio 79
 
+Problema:  Crie um programa onde o usuário possa digitar vários valores 
+           numéricos e cadastre-os em uma lista. Caso o número já exista 
+           lá dentro, ele não será adicionado. No final, serão exibidos todos 
+           os valores únicos digitados, em ordem crescente.
+           
+Resolucao do problema:
+'''
 lista = []
 
 while True:
 
     print('-='*20)
     valor = int(input('Digite um valor: '))
+
     if valor in lista:
         print('Erro! Digite outro valor')
 
@@ -15,6 +23,7 @@ while True:
         lista.append(valor)
 
     continua = str(input('Deseja continuar? [S/N]: ')).upper()
+    
     while continua not in 'SN':
         continua = str(input('Deseja continuar? [S/N]: ')).upper()
     

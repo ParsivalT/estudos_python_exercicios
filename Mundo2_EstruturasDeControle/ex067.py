dbe803ce23b6ef5d5adc1e400e46a1adec927eb2
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 14:59:03 -0300

    Tabuada v3.0

--- Mundo2_EstruturasDeControle/ex067.py
+++ Mundo2_EstruturasDeControle/ex067.py
@@ -1,9 +1,19 @@
-# Mostra a tabuada do numero digitado, Termina ao digitar um numero negativo!
+'''
+Desafio 67
+
+Problema: Faça um programa que mostre a tabuada de vários números, 
+          um de cada vez, para cada valor digitado pelo usuário. 
+          O programa será interrompido quando o número solicitado for negativo.
+          
+Resolucao do problema:
+'''
 numero = 0
 
 while True:
+    
     print('--'*10)
     numero = int(input('Digite: '))
+
     if numero >= 0:
         for c in range(0, 11):
             print(f'{numero} x {c} = {numero*c}')

146a2fb4ae680c32de5aa8db4f94e655c49e0a24
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 19:23:16 -0300

    Número por Extenso

--- Mundo3_EstruturasCompostas/ex072.py
+++ Mundo3_EstruturasCompostas/ex072.py
@@ -1,9 +1,18 @@
+'''
+Desafio 72 
+
+Problema: Crie um programa que tenha uma dupla totalmente preenchida com 
+          uma contagem por extenso, de zero até vinte. Seu programa deverá 
+          ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+          
+Resolucao do problema:
+'''
 print('='*31)
 numeros = ('Zero', 'Um', 'Dois', 'Tres', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezeseis', 'Dezessete', 'Dezoito','Dezenove', 'Vinte')
 numero = int(input('Digite um numero Entre 0 e 20: '))
 
 while numero > 20 or numero < 0:
-
+    
     print('Digite um valor valido!')
     numero = int(input('Digite um numero Entre 0 e 20: '))
 

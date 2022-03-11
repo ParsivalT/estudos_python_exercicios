67559e015e043dfefe952aebee5efa6c25222aab
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 02:17:47 -0300

    Contagem regressiva

--- Mundo2_EstruturasDeControle/ex046.py
+++ Mundo2_EstruturasDeControle/ex046.py
@@ -1,3 +1,11 @@
+'''
+Desafio 46
+
+Problema: Faça um programa que mostre na tela uma contagem regressiva para 
+          o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+          
+Resolucao do problema:
+'''
 from time import sleep 
 
 print('-='*30)
@@ -7,4 +15,5 @@ print('-='*30)
 for c in range(10, 0,-1):
     print(c)
     sleep(2)
+
 print('\033[36mFELIZ ANO NOVO\033[m')

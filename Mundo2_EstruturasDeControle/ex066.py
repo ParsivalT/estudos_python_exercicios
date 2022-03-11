ef0fe4da49c1bf59707e0c6e1cae0cb1d7bddb41
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 14:30:10 -0300

    Vários números com flag

--- Mundo2_EstruturasDeControle/ex066.py
+++ Mundo2_EstruturasDeControle/ex066.py
@@ -1,16 +1,26 @@
-numero =  0 
-total = 0 
-while True:
+'''
+Desafio 66
 
-    print('--'*10)
-    numero = int(input('Digite os numeros: '))
+Problema: Crie um programa que leia números inteiros pelo teclado. 
+          O programa só vai parar quando o usuário digitar o valor 999, 
+          que é a condição de parada. No final, mostre quantos números foram 
+          digitados e qual foi a soma entre elas (desconsiderando o flag).
+          
+Resolucao do problema: 
+'''
+numero = total = cont = 0 
+
+while True:
+    
+    numero = int(input('Digite os numeros(Digite 999 para sair): '))
+
     if numero != 999:
-        
         total += numero
+        cont += 1
     
     if numero == 999:
         break
 
-print('-=-'*10)
-print(f'A soma de  todos valores digitados: {total}')
-print('-=-'*10)
+print('=='*20)
+print(f'A soma dos {cont} valores digitados foi: {total}')
+print('=='*20)

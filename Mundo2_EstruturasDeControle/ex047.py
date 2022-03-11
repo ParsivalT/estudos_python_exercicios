2d760c8e7cff34af65745a5db60a674a79587693
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 02:22:04 -0300

    Contagem de pares

--- Mundo2_EstruturasDeControle/ex047.py
+++ Mundo2_EstruturasDeControle/ex047.py
@@ -1,5 +1,14 @@
+'''
+Desafio 47
 
+Problema: Crie um programa que mostre na tela todos os números pares 
+          que estão no intervalo entre 1 e 50.
+          
+Resolucao do problema:
+'''
 for c in range(0,50):
     p = c % 2 
+
     if p == 1:
        print(c) 
+ 

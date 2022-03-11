80428b69ebc1c8ffe34500e03f6bf3d0d9c56d44
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-11 02:30:21 -0300

    Tabuada v.2.0

--- Mundo2_EstruturasDeControle/ex049.py
+++ Mundo2_EstruturasDeControle/ex049.py
@@ -1,5 +1,14 @@
+'''
+Desafio 49
+
+Problema:  Refaça o DESAFIO 9, mostrando a tabuada de um número que 
+           o usuário escolher, só que agora utilizando um laço for.
+           
+Resolucao do problema: 
+'''
 print('-=-'*30)
 print('>>>>>TABUADA<<<<<')
+
 n = int(input('Digite O Numero Para ver A Tabuada Dele: '))
 
 for c in range(1,11):

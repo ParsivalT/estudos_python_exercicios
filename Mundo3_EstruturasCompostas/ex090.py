e378e6e244ea2c50b8c24fe29d6c87a9ca472803
ParsivalT <tthiagovinnicius@gmail.com>
2022-03-13 00:22:40 -0300

    Dicionário em Python

---
+++ Mundo3_EstruturasCompostas/ex090.py
@@ -0,0 +1,30 @@
+'''
+Desafio 90
+
+Problema: Faça um programa que leia nome e média de um aluno, guardando 
+          também a situação em um dicionário. No final, mostre o conteúdo 
+          da estrutura na tela.
+          
+Resolucao do problema:
+'''
+aluno = {}
+
+print('='*33)
+aluno['nome'] = str(input('Nome do Aluno(a): '))
+aluno['media'] = float(input('Media: '))
+
+if aluno['media'] >= 7:
+    aluno['estado'] = '\033[36mAprovado\033[m'
+
+elif aluno['media'] >= 5 and aluno['media'] < 7:
+    aluno['estado'] = '\033[32mRecuperacao\033[m'
+
+else:
+    aluno['estado'] = '\033[31mReprovado\033[m'
+
+print('='*33)
+
+print(f'- O nome do aluno: {aluno["nome"]}')
+print(f'- A media dele/dela foi: {aluno["media"]}')
+print(f'- Estado atual do aluno: {aluno["estado"]}')
+print('='*33)
